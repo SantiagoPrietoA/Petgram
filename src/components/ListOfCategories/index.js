020256fa5f9a33dabe075ca224c213b3ee8741ce
Santiago Prieto <santiagoprietoacero@gmail.com>
2020-02-07 17:28:49 -0500

    section hooks

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,17 +1,71 @@
-import React from "react";
+import React, { useEffect, useState, Fragment } from "react";
 import { Category } from "../Category";
-import { categories } from "../../../api/db.json";
 
 import { List, Item } from "./styles";
 
+function useCategoriesData() {
+  const [categories, setCategories] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const getCategories = async () => {
+    setLoading(true);
+    const response = await fetch(
+      "https://petgram-server-santiagoprieto-dm3pfhuwh.now.sh/categories"
+    );
+    const data = await response.json();
+    setCategories(data);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    const getData = async () => {
+      await getCategories();
+    };
+
+    getData();
+  }, []);
+
+  return { categories, loading };
+}
+
 export const ListOfCategories = () => {
+  const { categories, loading } = useCategoriesData();
+  const [showFixed, setShowFixed] = useState(false);
+
+  useEffect(() => {
+    const onScroll = e => {
+      const newShowFixed = window.scrollY > 190;
+      showFixed !== newShowFixed && setShowFixed(newShowFixed);
+    };
+
+    document.addEventListener("scroll", onScroll);
+
+    return () => document.removeEventListener("scroll", onScroll);
+  }, [showFixed]);
+
+  const renderList = fixed => {
+    return (
+      <List fixed={fixed}>
+        {loading ? (
+          <Item key="loading">
+            <Category />
+          </Item>
+        ) : (
+          categories.map(category => (
+            <Item key={category.id}>
+              <Category {...category} />
+            </Item>
+          ))
+        )}
+      </List>
+    );
+  };
+
   return (
-    <List>
-      {categories.map(category => (
-        <Item key={category.id}>
-          <Category {...category} />
-        </Item>
-      ))}
-    </List>
+    <Fragment>
+      {renderList()}
+      {/* {renderList(true)} */}
+      {showFixed && renderList(true)}
+    </Fragment>
   );
 };
--- src/components/ListOfCategories/styles.js
+++ src/components/ListOfCategories/styles.js
@@ -1,9 +1,44 @@
 import styled from "styled-components";
+import { keyframes, css } from "styled-components";
+
+const downKeyframes = keyframes`
+  from {
+    top: -100px
+  }
+
+  to {
+    top: -20px
+  }
+`;
+
+const downUp = ({ time = ".5s", type = "ease" } = {}) =>
+  css`
+    animation: ${time} ${downKeyframes} ${type};
+  `;
 
 export const List = styled.ul`
   display: flex;
   overflow: scroll;
   width: 100%;
+  ${props =>
+    props.fixed &&
+    css`
+       {
+        ${downUp()}
+        background: #fff;
+        border-radius: 60px;
+        box-shadow: 0 0 20px rgba(0, 0, 0, 0.3);
+        left: 0;
+        margin: 0 auto;
+        max-width: 400px;
+        padding: 5px;
+        position: fixed;
+        right: 0;
+        top: -20px;
+        transform: scale(0.5);
+        z-index: 1;
+      }
+    `}
 `;
 
 export const Item = styled.li`
