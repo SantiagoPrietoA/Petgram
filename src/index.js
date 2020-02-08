812b5789e89d9668e590406c4bfaa8720f866ff6
Santiago Prieto <santiagoprietoacero@gmail.com>
2020-02-07 22:55:27 -0500

    data with graphQL

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+
 import { App } from "./App";
 
-const app = document.getElementById("app");
+const client = new ApolloClient({
+  uri: "https://petgram-server-santiagoprieto-dm3pfhuwh.now.sh/graphql"
+});
 
-ReactDOM.render(<App />, app);
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("app")
+);
