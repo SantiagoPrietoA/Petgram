fe23dae757fc5bba63011fcc808753371bc43b22
Santiago Prieto <santiagoprietoacero@gmail.com>
2020-02-09 15:11:57 -0500

    user management

--- src/index.js
+++ src/index.js
@@ -2,16 +2,35 @@ import React from "react";
 import ReactDOM from "react-dom";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
+import Context from "./Context";
 
 import { App } from "./App";
 
 const client = new ApolloClient({
-  uri: "https://petgram-server-santiagoprieto-dm3pfhuwh.now.sh/graphql"
+  uri: "https://petgram-server-santiagoprieto-dm3pfhuwh.now.sh/graphql",
+  request: operation => {
+    const token = sessionStorage.getItem("token");
+    const authorization = token ? `bearer ${token}` : "";
+    operation.setContext({
+      headers: {
+        authorization
+      }
+    });
+  },
+  onError: error => {
+    const { networkError } = error;
+    if (networkError && networkError.result.code === "invalid_token") {
+      sessionStorage.removeItem("token");
+      window.location.href = "/";
+    }
+  }
 });
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
+  <Context.Provider>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </Context.Provider>,
   document.getElementById("app")
 );
